d57586beb354ebde92fe902b7410dab6bfcded16
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-10-13 07:15:33 +0900

    Force 'Stay logged in' checkbox to be checked

---
+++ app/overrides/account/login.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(:virtual_path => 'account/login',
+                     :name => "force autologin checkbox to checked=true",
+                     :insert_after => "erb[loud]:contains('call_hook :view_account_login_bottom')") do
+"""
+<%= javascript_tag \"$('#autologin').prop('checked', true);\" %>
+"""
+end

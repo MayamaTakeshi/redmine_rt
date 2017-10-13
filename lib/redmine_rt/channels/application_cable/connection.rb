d0aae7c7cc3a8e07700fc41794bdf08d447f0d2c
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-10-13 15:24:10 +0900

    Added support for API access_key for ActionCable

--- lib/redmine_rt/channels/application_cable/connection.rb
+++ lib/redmine_rt/channels/application_cable/connection.rb
@@ -8,13 +8,23 @@ module ApplicationCable
 
     protected
       def find_verified_user
-        token = Token.find_by(action: :autologin, value: cookies[:autologin])
+        token = nil
+
+        if request.params[:access_key] then
+          token = Token.find_by(action: :api, value: request.params[:access_key])
+        end
+
+        if not token then
+          token = Token.find_by(action: :autologin, value: cookies[:autologin])
+        end
+
         if not token then
           request.params[:unauthorized] = true
           return
 	end
+
         if verified_user = User.find(token.user_id) then
-          verified_user
+          return verified_user
         else
           request.params[:unauthorized] = true
           return verified_user

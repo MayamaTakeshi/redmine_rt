5d0cbf85c132023d5bbed766c188229a61b3d62f
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-10-13 16:04:23 +0900

    cosmetic changes

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,8 +2,6 @@ class ChannelsController < ApplicationController
   accept_api_auth :post_msg
 
   def post_msg
-    puts "post_message"
-    puts params
     RedmineRt::Broadcaster.broadcast params[:id], params[:msg]
     render status: 204, body: nil
   end

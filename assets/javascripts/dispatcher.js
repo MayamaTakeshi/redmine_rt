84a7da4be981c97ffe129eb5eb8ba46f31b29647
root <root@debian1.localdomain>
2017-11-11 07:38:18 +0000

    Corrected websocket base_url resolution

--- assets/javascripts/dispatcher.js
+++ assets/javascripts/dispatcher.js
@@ -6,8 +6,9 @@
  
   var setup = function(event_handler) {
     var channel_name = $('meta[name=page_specific_js]').attr('channel_name');
+    var base_url = window.location.href.split("/issues/")[0].split("://")[1];
     console.log("Opening websocket-rails private channel " + channel_name);
-    App.dispatcher = new WebSocketRails(window.location.host + '/websocket');
+    App.dispatcher = new WebSocketRails(base_url + '/websocket');
     var private_channel = App.dispatcher.subscribe_private(channel_name, function(current_user) {
       console.log('Websocket-Rails connection and subscription successful');
       private_channel.bind('ALL', event_handler);
--- lib/redmine_rt/issues_controller_patch.rb
+++ lib/redmine_rt/issues_controller_patch.rb
@@ -2,6 +2,10 @@ module RedmineRt
   # Patches Redmine's IssuesController. 
   module IssuesControllerPatch
 
+    def sample_helper_method
+      return "some_result"
+    end
+
     def self.included(base) # :nodoc:
       base.extend(ClassMethods)
 
@@ -12,6 +16,8 @@ module RedmineRt
 
         before_action :find_issue, :only => [:show, :edit, :update, :add_quick_notes]
         before_action :authorize, :except => [:index, :new, :create, :add_quick_notes]
+
+	helper_method :sample_helper_method # this method can be called from an issues view.
       end
 
     end
@@ -29,6 +35,8 @@ module RedmineRt
         save_issue_with_child_records
         render json: "{}"
       end
+
     end
+
   end
 end

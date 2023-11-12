c09027f047080c93aec272843920011a86d55bf1
MayamaTakeshi <mayamatakeshi@gmail.com>
2023-11-12 15:10:28 +0900

    Simplified some modules

--- lib/redmine_rt/issue_patch.rb
+++ lib/redmine_rt/issue_patch.rb
@@ -1,24 +1,12 @@
 module RedmineRt
-  # Patches Redmine's Issue model. 
   module IssuePatch
     def self.included(base) # :nodoc:
-      base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
-
-      # Same as typing in the class
       base.class_eval do
-        unloadable # Send unloadable so it will not be unloaded in development
-
         after_save :handle_issue_after_save
         after_destroy :handle_issue_after_destroy
       end
     end
-  end
 
-  module ClassMethods
-  end
-
-  module InstanceMethods
     def handle_issue_after_save
       Broadcaster.broadcast "issue:#{self.id}", 
         { event: 'issue ' + self.id.to_s +  ' saved', type: 'issue_saved', issue_id: self.id }
--- lib/redmine_rt/journal_patch.rb
+++ lib/redmine_rt/journal_patch.rb
@@ -1,26 +1,13 @@
 module RedmineRt
-  # Patches Redmine's Journal model. 
   module JournalPatch
     def self.included(base) # :nodoc:
-      base.extend(ClassMethods)
-
-      base.send(:include, InstanceMethods)
-
-      # Same as typing in the class
       base.class_eval do
-        unloadable # Send unloadable so it will not be unloaded in development
-
         after_create :handle_journal_after_create
         after_save :handle_journal_after_save
         after_destroy :handle_journal_after_destroy
       end
     end
-  end
 
-  module ClassMethods
-  end
-
-  module InstanceMethods
     def handle_journal_after_create
       logger.debug "handle_journal_after_create"
       issue = Issue.find(self.journalized_id)
@@ -45,6 +32,7 @@ module RedmineRt
       Broadcaster.broadcast "issue:#{self.journalized_id}",
         { event: 'journal ' + self.id.to_s +  ' saved', type: 'journal_saved', journal_id: self.id }
     end
+
     def handle_journal_after_destroy
       logger.debug "handle_journal_after_destroy"
       if self.journalized_type != 'Issue' then return end
